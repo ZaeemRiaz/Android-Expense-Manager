03ffd0078cad76e28b43ca62fb7d902c8a886d38
Mohammad Mannan Akmal <mmannanakmal@gmail.com>
2020-05-18 22:13:12 +0500

    Time and Date Dialog

--- app/src/main/java/com/base/software_for_mobile_devices_project/TransactionAddEditActivity.java
+++ app/src/main/java/com/base/software_for_mobile_devices_project/TransactionAddEditActivity.java
@@ -1,16 +1,42 @@
 package com.base.software_for_mobile_devices_project;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.Calendar;
+
 public class TransactionAddEditActivity extends AppCompatActivity {
 
     private static final String TAG = "=== TransactionAddEditActivity ===";
+    private final DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            TextView date = findViewById(R.id.date_text_view);
+            String strDate = dayOfMonth + "-" + (month + 1) + "-" + year;
+            date.setText(strDate);
+        }
+    };
+
+    private final TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            TextView time = findViewById(R.id.date_text_view2);
+            String strTime = hourOfDay + ":" + minute;
+            time.setText(strTime);
+        }
+    };
+
     private TabLayout tabLayout;
     private ViewPager viewPager;
 
@@ -21,8 +47,8 @@ public class TransactionAddEditActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_transaction_add_edit);
 
-        viewPager = (ViewPager) findViewById(R.id.pager);
-        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        viewPager = findViewById(R.id.pager);
+        tabLayout = findViewById(R.id.tabs);
 
         TransactionPagerAdapter transactionPagerAdapter = new TransactionPagerAdapter(
                 getSupportFragmentManager(), tabLayout.getTabCount());
@@ -32,8 +58,24 @@ public class TransactionAddEditActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
 
 
-
     }
 
+    public void openDatePickerDialog(View view) {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
 
+        DatePickerDialog dialog = new DatePickerDialog(this, dateSetListener, year, month, day);
+        dialog.show();
+    }
+
+    public void openTimePickerDialog(View view) {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        TimePickerDialog dialog = new TimePickerDialog(this, timeSetListener, hour, minute, false);
+        dialog.show();
+    }
 }
